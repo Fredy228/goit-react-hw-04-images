43253c700ee7225b2161df67d92bfe18f6b350cb
Fredy228 <smagrovich58@gmail.com>
2023-02-06 21:04:23 +0200

    searchbar-refactored

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "./App.styled";
-import Searchbar from 'components/Searchbar/Searchbar';
+import {Searchbar} from 'components/Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import { Button } from "components/Button/Button";
 import Modal from "components/Modal/Modal";
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,45 @@
-import React from "react";
+import {useState} from "react";
 import PropTypes from 'prop-types';
 import { BarBlock, SearchForm, SearchFormButton, SearchFormInput, SearchFormButtonLabel } from "./Searchbar.styled";
 
-class Searchbar extends React.Component {
-    state = {
-        query: ''
-    }
+export const Searchbar = ({onSubmitForm}) => {
 
-    handleChange = event => {
+    const [query, setQuery] = useState('');
+
+    const handleChange = event => {
         const {value} = event.currentTarget;
-        this.setState({query: value})
+        setQuery(value);
       }
 
-    submitForm = (event) => {
+    const submitForm = event => {
         event.preventDefault();
-        if(this.state.query.trim() !== '') {
-            this.props.onSubmitForm(this.state.query)
+        if(query.trim() !== '') {
+            onSubmitForm(query);
         } else {
-            window.alert('Enter the query')
+            window.alert('Enter the query');
         }
-        event.currentTarget.reset()
+        event.currentTarget.reset();
     }
 
-    render () {
-        return (
-            <BarBlock>
-                <SearchForm onSubmit={this.submitForm}>
-                    <SearchFormButton type="submit">
-                        <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-                    </SearchFormButton>
-                    <SearchFormInput 
-                    onChange={this.handleChange} 
-                    placeholder={'Search images and photos'}
-                    type="text"
-                    autocomplete="off"
-                    autoFocus/>
-                </SearchForm>
-            </BarBlock>
-        )
-    }
+    
+    return (
+        <BarBlock>
+            <SearchForm onSubmit={submitForm}>
+                <SearchFormButton type="submit">
+                    <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+                </SearchFormButton>
+                <SearchFormInput 
+                onChange={handleChange} 
+                placeholder={'Search images and photos'}
+                type="text"
+                autocomplete="off"
+                autoFocus/>
+            </SearchForm>
+        </BarBlock>
+    )
+    
 }
 
 Searchbar.propTypes = {
     onSubmitForm: PropTypes.func.isRequired
 }
-
-export default Searchbar;
